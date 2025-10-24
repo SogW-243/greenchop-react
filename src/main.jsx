4d0a9ad553c38a04d252e100bc65f93debd3adfe
SogW <nguyenmanhquynh6324@gmail.com>
2025-10-24 15:10:45 +0700

    Fix: Cấu hình base path và basename cho GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-// src/main.jsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { store } from "./app/store"; // Đường dẫn store của bạn
 import App from "./App";
 import "./index.css";
+// 1. Import BrowserRouter
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/greenchop-react">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path"; // <--- Thêm dòng này
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/greenchop-react/",
   resolve: {
     // <--- Thêm toàn bộ khối "resolve" này
     alias: {
